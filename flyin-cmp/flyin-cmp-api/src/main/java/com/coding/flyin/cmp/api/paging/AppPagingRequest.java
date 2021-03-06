fdf15ed1511554529371d4d19ad48f255a0e76af
emon <liming20110711@163.com>
2021-03-07 00:10:52 +0800

    perf:修复分页请求可以传递无效参数的bug

--- flyin-cmp/flyin-cmp-api/src/main/java/com/coding/flyin/cmp/api/paging/AppPagingRequest.java
+++ flyin-cmp/flyin-cmp-api/src/main/java/com/coding/flyin/cmp/api/paging/AppPagingRequest.java
@@ -15,4 +15,14 @@ public class AppPagingRequest extends AppRequest {
 
     /** 分页尺寸：必须大于0. */
     protected int pageSize = 20;
+
+    public void setPageIndex(int pageIndex) {
+        // 如果当前页小于0，重置为0，0表示第一页
+        this.pageIndex = Math.max(pageIndex, 0);
+    }
+
+    public void setPageSize(int pageSize) {
+        // 如果页面尺寸小于1，重置为1，最小是1条数据一页;
+        this.pageSize = Math.max(pageSize, 1);
+    }
 }
