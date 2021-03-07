c7f7f617d7d1828c7157533f58202a8c6c14f669
emon <liming20110711@163.com>
2021-03-07 11:54:07 +0800

    perf:优化非幂等性接口校验逻辑

--- flyin-cmp/flyin-cmp-idempotent/src/main/java/com/coding/flyin/cmp/idempotent/NonIdempotentSupport.java
+++ flyin-cmp/flyin-cmp-idempotent/src/main/java/com/coding/flyin/cmp/idempotent/NonIdempotentSupport.java
@@ -5,7 +5,7 @@ import com.coding.flyin.cmp.exception.AppStatus;
 import com.coding.flyin.cmp.idempotent.annotation.resolver.config.NonIdempotentConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.redisson.api.RSetCache;
+import org.redisson.api.RMapCache;
 import org.redisson.api.RedissonClient;
 import org.redisson.client.codec.Codec;
 import org.redisson.codec.KryoCodec;
@@ -35,20 +35,31 @@ public class NonIdempotentSupport {
 
     private static final Codec codec = new KryoCodec();
 
-    /** 根据标志获取requestId，如果已经存在了一份identity的数据则抛异常. */
+    /** 根据标志获取requestId，冷却时间是1秒，1秒之内不允许获取第二次；如果已经存在了一份identity的数据则抛异常. */
     public static String generateRequestId(String identity) {
-        RSetCache<String> setCache = redisson.getSetCache(nonIdempotentConfig.getRedisKey(), codec);
+        return generateRequestId(identity, 1, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 生成非幂等性接口所需参数 requestId
+     *
+     * @param identity - 根据标志获取requestId，如果已经存在了一份identity的数据则抛异常.
+     * @param coolDownTime - 冷却时间，多长时间允许生成一次
+     * @param timeUnit - 时间单位
+     * @return
+     */
+    public static String generateRequestId(String identity, long coolDownTime, TimeUnit timeUnit) {
+        RMapCache<String, String> mapCache =
+                redisson.getMapCache(nonIdempotentConfig.getRedisKey(), codec);
         if (StringUtils.isNotEmpty(identity)) {
-            if (!setCache.add(identity, 1, TimeUnit.SECONDS)) {
+            if (mapCache.containsKey(identity)) {
                 log.error("【幂等校验】获取requestId太过频繁 identity={}", identity);
                 throw new AppException(AppStatus.U0507, "获取requestId太过频繁");
             }
+            mapCache.put(identity, identity, coolDownTime, timeUnit);
         }
-        String requestId = UUID.randomUUID().toString();
-        if (!setCache.add(requestId, 5, TimeUnit.MINUTES)) {
-            log.error("【幂等校验】获取requestId失败，服务端保存失败");
-            throw new AppException(AppStatus.U0507, "获取requestId失败");
-        }
+        String requestId = UUID.randomUUID().toString().replace("-", "");
+        mapCache.put(requestId, requestId, 5, TimeUnit.MINUTES);
         return requestId;
     }
 
@@ -57,8 +68,9 @@ public class NonIdempotentSupport {
             log.warn("【幂等校验】requestId为空，默认校验结果为 false");
             return false;
         } else {
-            RSetCache<String> setCache = redisson.getSetCache(nonIdempotentConfig.getRedisKey(), codec);
-            return setCache.remove(requestId);
+            RMapCache<String, String> mapCache =
+                    redisson.getMapCache(nonIdempotentConfig.getRedisKey(), codec);
+            return mapCache.remove(requestId, requestId);
         }
     }
 }
--- flyin-cmp/flyin-cmp-idempotent/src/main/java/com/coding/flyin/cmp/idempotent/annotation/resolver/NonIdempotentResolver.java
+++ flyin-cmp/flyin-cmp-idempotent/src/main/java/com/coding/flyin/cmp/idempotent/annotation/resolver/NonIdempotentResolver.java
@@ -2,6 +2,7 @@ package com.coding.flyin.cmp.idempotent.annotation.resolver;
 
 import com.coding.flyin.cmp.api.AppRequest;
 import com.coding.flyin.cmp.api.AppResponse;
+import com.coding.flyin.cmp.exception.AppException;
 import com.coding.flyin.cmp.exception.AppStatus;
 import com.coding.flyin.cmp.idempotent.NonIdempotentSupport;
 import com.coding.flyin.core.annotation.support.TargetPoint;
@@ -12,6 +13,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Aspect
 @Component
@@ -20,9 +22,8 @@ import org.springframework.stereotype.Component;
 public class NonIdempotentResolver {
 
     @Around(
-        value =
-                "@annotation(com.coding.flyin.cmp.idempotent.annotation.NonIdempotent) || @within(com.coding.flyin.cmp.idempotent.annotation.NonIdempotent)"
-    )
+            value =
+                    "@annotation(com.coding.flyin.cmp.idempotent.annotation.NonIdempotent) || @within(com.coding.flyin.cmp.idempotent.annotation.NonIdempotent)")
     public Object doValidate(ProceedingJoinPoint point) throws Throwable {
         TargetPoint targetPoint = TargetPoint.createTargetPoint(point);
         Object[] args = targetPoint.getArgs();
@@ -31,14 +32,24 @@ public class NonIdempotentResolver {
                 if (AppRequest.class.isAssignableFrom(arg.getClass())) {
                     AppRequest request = (AppRequest) arg;
                     String requestId = request.getRequestId();
-                    boolean validateResult = NonIdempotentSupport.validateRequestId(requestId);
+                    boolean validateResult;
+                    String errorMessage;
+                    if (StringUtils.isEmpty(requestId)) {
+                        validateResult = false;
+                        errorMessage = "非幂等接口调用时需要 requestId 参数！";
+                    } else {
+                        validateResult = NonIdempotentSupport.validateRequestId(requestId);
+                        errorMessage = "不允许重复提交！";
+                    }
                     if (!validateResult) {
                         if (AppResponse.class.isAssignableFrom(targetPoint.getReturnType())) {
                             AppResponse response =
                                     (AppResponse) targetPoint.getReturnType().newInstance();
                             response.setErrorCode(AppStatus.U0506.getErrorCode());
-                            response.setErrorMessage("不允许重复提交！");
+                            response.setErrorMessage(errorMessage);
                             return response;
+                        } else {
+                            throw new AppException(AppStatus.U0506, errorMessage);
                         }
                     }
                 }
