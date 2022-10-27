7c0858816984fea35c11bb29532bdf480e0c5b59
emon <liming20110711@163.com>
2022-10-27 20:15:02 +0800

    feat:为延迟队列增加traceId保存的能力

--- flyin-starter/flyin-timer-spring-boot-starter/src/main/java/com/coding/flyin/starter/timer/delay/queue/AbstractShareDelayedQueue.java
+++ flyin-starter/flyin-timer-spring-boot-starter/src/main/java/com/coding/flyin/starter/timer/delay/queue/AbstractShareDelayedQueue.java
@@ -1,16 +1,21 @@
 package com.coding.flyin.starter.timer.delay.queue;
 
-import com.coding.flyin.starter.timer.PooledTimerTaskProperties;
-import com.coding.flyin.starter.timer.delay.DelayTask;
-import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 import org.redisson.api.RBlockingQueue;
 import org.redisson.api.RDelayedQueue;
+import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
+import org.slf4j.MDC;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.util.StringUtils;
 
-import java.util.concurrent.TimeUnit;
+import com.coding.flyin.starter.timer.PooledTimerTaskProperties;
+import com.coding.flyin.starter.timer.delay.DelayTask;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public abstract class AbstractShareDelayedQueue implements DelayedQueue {
@@ -25,6 +30,9 @@ public abstract class AbstractShareDelayedQueue implements DelayedQueue {
 
     private final String logPrefix;
 
+    private final RedissonClient redissonClient;
+    private final String mdcRedisKey;
+
     AbstractShareDelayedQueue(
             PooledTimerTaskProperties.Delay delay,
             ThreadPoolTaskExecutor delayPoolQueueExecutor,
@@ -38,6 +46,8 @@ public abstract class AbstractShareDelayedQueue implements DelayedQueue {
             logPrefix = delay.getDelayTaskLogPrefix();
         }
         this.logPrefix = logPrefix;
+        this.redissonClient=redissonClient;
+        this.mdcRedisKey = delay.getDelayTaskQueueRedisKey().concat("MDC");
 
         this.init();
     }
@@ -74,6 +84,7 @@ public abstract class AbstractShareDelayedQueue implements DelayedQueue {
     @Override
     public void put(@NonNull DelayTask delayTask, long timeout, @NonNull TimeUnit timeUnit) {
         DelayMDCTaskDecorator decorate = new DelayMDCTaskDecorator(delayTask);
+        storeMDC(delayTask);
         log.info("【{}】任务已加入延迟队列,taskId={}", logPrefix, decorate.getTaskId());
         delayedQueue.offer(decorate, timeout, timeUnit);
     }
@@ -83,6 +94,7 @@ public abstract class AbstractShareDelayedQueue implements DelayedQueue {
             @NonNull DelayTask delayTask, long timeout, @NonNull TimeUnit timeUnit) {
         DelayMDCTaskDecorator decorate = new DelayMDCTaskDecorator(delayTask);
         if (!delayedQueue.contains(decorate)) {
+            storeMDC(delayTask);
             log.info("【{}】任务已加入延迟队列,taskId={}", logPrefix, decorate.getTaskId());
             delayedQueue.offer(decorate, timeout, timeUnit);
             return true;
@@ -95,6 +107,7 @@ public abstract class AbstractShareDelayedQueue implements DelayedQueue {
     @Override
     public Boolean remove(@NonNull DelayTask delayTask) {
         DelayMDCTaskDecorator decorate = new DelayMDCTaskDecorator(delayTask);
+        clearMDC(delayTask);
         boolean success = delayedQueue.remove(decorate);
         if (success) {
             log.info("【{}】任务已剔除出延迟队列,taskId={}", logPrefix, decorate.getTaskId());
@@ -112,6 +125,7 @@ public abstract class AbstractShareDelayedQueue implements DelayedQueue {
             boolean success;
             do {
                 success = delayedQueue.remove(decorate);
+                clearMDC(delayTask);
                 if (success) {
                     removeCount++;
                 }
@@ -140,6 +154,7 @@ public abstract class AbstractShareDelayedQueue implements DelayedQueue {
             // 阻塞式获取
             try {
                 DelayTask task = blockingFairQueue.take();
+                loadMDC(task);
                 log.info("【分布式延时任务队列】任务已提取并加入线程池,taskId={}", task.getTaskId());
                 delayPoolQueueExecutor.execute(task);
             } catch (InterruptedException e) {
@@ -148,5 +163,33 @@ public abstract class AbstractShareDelayedQueue implements DelayedQueue {
             }
         }
         log.info("【分布式{}守护线程】关闭,thread={}", logPrefix, Thread.currentThread().getId());
+    }
+
+
+
+    private void storeMDC(DelayTask delayTask) {
+        Map<String, String> MDCEnvironment = MDC.getCopyOfContextMap();
+        if (MDCEnvironment != null) {
+            RMap<String, Map<String, String>> mdcEnv = redissonClient.getMap(mdcRedisKey);
+            mdcEnv.put(delayTask.getTaskId(), MDCEnvironment);
+        }
+    }
+
+    private void clearMDC(DelayTask delayTask) {
+        Map<String, String> MDCEnvironment = MDC.getCopyOfContextMap();
+        if (MDCEnvironment != null) {
+            RMap<String, Map<String, String>> mdcEnv = redissonClient.getMap(mdcRedisKey);
+            mdcEnv.remove(delayTask.getTaskId());
+        }
+    }
+
+    private void loadMDC(DelayTask delayTask) {
+        RMap<String, Map<String, String>> mdcEnv = redissonClient.getMap(mdcRedisKey);
+        if (mdcEnv.containsKey(delayTask.getTaskId())) {
+            Map<String, String> MDCEnvironment = mdcEnv.remove(delayTask.getTaskId());
+            if (delayTask instanceof DelayMDCTaskDecorator) {
+                ((DelayMDCTaskDecorator)delayTask).setMDCEnvironment(MDCEnvironment);
+            }
+        }
     }
 }
